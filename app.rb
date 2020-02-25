0717d1d4afcd32ee809263297351900bd5c7f26f
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-02-25 22:53:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,14 +12,16 @@ module FormsLab
    end 
    
    post '/pirates' do 
-     @pirate = Pirate.new(params[:pirate])
-     
-     params[:pirate][:ship].each do |info|
-       Pirate.new(info)
-     end 
-     @ships = Ships.all
-   end 
-  erb :'pirates/show'
+    @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |info|
+        Ship.new(info)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
   end
  end
 end 
+
+
