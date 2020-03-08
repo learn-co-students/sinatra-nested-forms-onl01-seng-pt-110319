2a0a7205408c5164bebd51c5e482451d585162a3
mttwright <mttwright@gmail.com>
2020-03-08 11:22:21 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+    @@all = []
+    attr_reader :name, :weight, :height
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+    @@all = []
+    attr_reader :name, :type, :booty
+
+    def initialize(hash)
+        @name = hash[:name]
+        @type = hash[:type]
+        @booty = hash[:booty]
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
+
 end
